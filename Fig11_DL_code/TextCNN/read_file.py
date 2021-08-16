82d6c9fb689ac8940bcd56cad1df6b8488cab511
vyqrvwgf1 <353351559@qq.com>
2021-08-16 18:51:07 +0800

    Delete Fig11_DL_code directory

--- Fig11_DL_code/TextCNN/read_file.py
+++
@@ -1,9 +0,0 @@
-# 引入 word2vec
-from gensim.models import word2vec
-import numpy as np
-import data_helpers as dh
-# 引入日志配置
-import logging
-from tensorflow.contrib import learn
-from text_cnn import *
-config=TextConfig()
